650925c21ae3546310045f67c6d9d815d5b84147
Luis Miguel Cabezas <luismiguel.cabezas@gmail.com>
2022-04-18 11:19:47 +0200

    Configurado REDIS

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
-  root "cities#resultados"
-
   get 'cities/resultados'
 
   resources :cities
+
+  root "cities#resultados"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
